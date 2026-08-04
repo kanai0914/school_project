0a2b4b66c7cdc4456f11ce64aa687e0844658234
kni-0830 <ayuayugonbe@gmail.com>
2026-08-04 15:45:44 +0900

    feat:main

---
+++ main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int task_num;
+
+int select_task(int task)
+{
+    switch (task)
+    {
+    case 1:
+        printf("1\n");
+        break;
+
+    case 2:
+        printf("2\n");
+        break;
+
+    case 3:
+        printf("3\n");
+        break;
+
+    case 4:
+        printf("4\n");
+        break;
+
+    default:
+        printf("sorry\n");
+        break;
+    }
+}
+
+int main(void)
+{
+    printf("please enter a number >>");
+    scanf("%d", &task_num);
+    select_task(task_num);
+    return 0;
+}
