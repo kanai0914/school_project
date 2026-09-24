453f51d8a58340c88ac19dc35440e7f7278fe040
kanai0914 <ayuayugonbe@gmail.com>
2026-09-25 04:38:47 +0900

    feat:checkする処理を関数化

--- main.c
+++ main.c
@@ -77,7 +77,7 @@ static double read_double_format(const char *format, ...)
 }
 
 // 安全にintの値を読む関数
-static int read_int(const char *format, ...)
+static int read_int_format(const char *format, ...)
 {
     int value;
     while (1)
@@ -88,7 +88,6 @@ static int read_int(const char *format, ...)
         va_end(args);
 
         int result = scanf("%d", &value);
-
         if (result == 1)
         {
             clear_input_buffer();
@@ -104,154 +103,71 @@ static int read_int(const char *format, ...)
     }
 }
 
-void check_value(double array[], int count, ...)
+static void check_values(double values[], const char *names[], int count)
 {
-
-    va_list args;
-    va_start(args, count);
-    char sentence[count];
-    for (int i = 0; i < count; i++)
+    while (1)
     {
-        sentence[i] = va_arg(args, char);
-    }
-    va_end(args);
-
-    printf("Are you ok this value?\n");
-    for (int i = 0; i < count; i++)
-    {
-        printf("%s>%d\n", sentence[i], array[i]);
-    }
-    printf("if YES. please enter 1.\n if NO. please enter 0.\n");
-    int decision = read_int("enter number>");
-
-    if (decision == 1)
-    {
+        printf("are you ok this value?\n");
         for (int i = 0; i < count; i++)
         {
-            printf("%s>%d\n", sentence[i], array[i]);
+            printf("%s>%lf\n", names[i], values[i]);
         }
-    }
-    else if (decision == 0)
-    {
-        printf("please select change resistance value\n");
-        printf("enter_number:select_value\n");
-        for (int i = 0; i < count; i++)
-        {
-            printf("%d:%s ", i + 1, sentence[i]);
-        }
-        printf("%d:all change\n", count + 1);
-        int select_number = read_int("enter number>");
+        printf("if YES. please enter 1.\n if NO. please enter 0.\n");
+        int decision = read_int_format("enter number>");
 
-        for (int i = 0; i < count; i++)
+        if (decision == 1)
         {
-            if (select_number = count + 1)
+            return;
+        }
+        else if (decision == 0)
+        {
+            printf("please select change value\n");
+            for (int i = 0; i < count; i++)
             {
-                for (int j = 0; j < count; j++)
+                printf("%d:%s  ", i + 1, names[i]);
+            }
+            printf("%d:All\n", count + 1);
+            int select_number = read_int_format("enter number>");
+
+            if (select_number == count + 1) // 全部変える
+            {
+                for (int i = 0; i < count; i++)
                 {
-                    array[j] = read_double_format("enter%s", sentence[j]);
+                    values[i] = read_double_format("%s>", names[i]);
                 }
-                break;
             }
-            if (select_number = i + 1)
+            else if (select_number >= 1 && select_number <= count) // 1個だけ変える
             {
-                array[i] = read_double_format("enter%s", sentence[i]);
-                break;
+                values[select_number - 1] = read_double_format("%s>", names[select_number - 1]);
+            }
+            else
+            {
+                printf("please enter a number from 1 to %d.\n", count + 1);
             }
         }
-    }
-    else
-    {
-        printf("Please enter a value 0 or 1");
+        else
+        {
+            printf("Please enter a value 0 or 1\n");
+        }
     }
 }
 
 int main(void)
 {
-    double r1, r2, r3;
     double r[3];
+    const char *names[3] = {"R1", "R2", "R3"};
 
-    // 抵抗の入力
     printf("please enter three resistance value\n");
-    r1 = read_double_format("R1:");
-    r2 = read_double_format("R2:");
-    r3 = read_double_format("R3:");
-
-    // 入力した抵抗が本当に大丈夫なのか確認
-    while (1)
+    for (int i = 0; i < 3; i++)
     {
-        printf("are you ok this value?\n R1>%lf\nR2>%lf\nR3>%lf\n", r1, r2, r3);
-        printf("if YES. please enter 1.\n if NO. please enter 0.\n");
-        int decision = read_int("enter number>");
-
-        if (decision == 1)
-        {
-            printf("R1:%lf\nR2:%lf\nR3:%lf\n", r1, r2, r3);
-            break;
-        }
-        else if (decision == 0)
-        {
-            printf("please select change resistance value\n");
-            printf("enter_number:select_value\n");
-            printf("1:R1  2:R2  3:R3 4:All resistance\n");
-            int select_number = read_int("enter number>");
-
-            switch (select_number)
-            {
-            case 1:
-                r1 = read_double_format("R1>");
-                break;
-            case 2:
-                r2 = read_double_format("R2>");
-                break;
-            case 3:
-                r3 = read_double_format("R3>");
-                break;
-            case 4:
-                r1 = read_double_format("R1>");
-                r2 = read_double_format("R2>");
-                r3 = read_double_format("R3>");
-                break;
-            default:
-                printf("Please enter a value between 1 and 4.");
-            }
-        }
-        else
-        {
-            printf("Please enter a value 0 or 1");
-        }
+        r[i] = read_double_format("%s:", names[i]);
     }
 
-    printf("please select experiment number.\n");
-    int experiment_number = read_int("range is 2 ~ 6>");
-    switch (experiment_number)
+    check_values(r, names, 3);
+
+    for (int i = 0; i < 3; i++)
     {
-    case 2:
-        printf("Please enter the information required to create Table 2.1.\n");
-        printf("first,please enter r1 current value");
-        double a2, a4, a6, a8;
-        a2 = read_double_format("voltage>2 current>");
-        a4 = read_double_format("voltage>4 current>");
-        a6 = read_double_format("voltage>6 current>");
-        a8 = read_double_format("voltage>8 current>");
-
-        break;
-    case 3:
-        break;
-    case 4:
-        break;
-    case 5:
-        break;
-    case 6:
-        break;
-    default:
-        break;
+        printf("%s:%lf\n", names[i], r[i]);
     }
-
-    /*
-    // 並列・直列の確認
-    printf("parallel: %.5lf\n", parallel_calculations(3, r1, r2, r3));
-    printf("series  : %.5lf\n", series_calculations(3, r1, r2, r3));
-    */
-
     return 0;
 }
